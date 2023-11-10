0b25c7a208fc9df7018e9a3dcb3991f999bb951f
Yumicode <75518128+helene-nguyen@users.noreply.github.com>
2023-11-10 06:27:02 +0100

    [exercise] add another solution

--- treasure_map.py
+++ treasure_map.py
@@ -9,7 +9,6 @@ print("Hiding your treasure! X marks the spot")
 
 position = input()
 
-# TODO
 for index, line in enumerate(map):
     for i, place  in enumerate(line):
         if position[0].capitalize() == 'A' and i+1 == int(position[1]):
@@ -18,9 +17,18 @@ for index, line in enumerate(map):
             line2[i] = 'X'
         elif position[0].capitalize() == 'C' and i+1 == int(position[1]):
             line3[i] = 'X'
-        else: 
-            print("Invalid position")
-            exit(0)
+        # else: 
+        #     print("Invalid position")
+        #     exit(0)
 
+
+print(f'Treasure hidden!\n{line1}\n{line2}\n{line3}')
+
+# ANOTHER SOLUTION
+letter = position[0].capitalize()
+abc = ["A", "B", "C"]
+letter_index = abc.index(letter)
+number_index = int(position[1]) - 1
+map[number_index][letter_index] = "X"
 
 print(f'Treasure hidden!\n{line1}\n{line2}\n{line3}')
